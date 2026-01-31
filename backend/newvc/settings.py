990df6338ff1b10774f8da991fae03be66aee71b
Pavel5553005 <pavel5553005@gmail.com>
2026-02-01 00:13:12 +0700

    add docker

--- backend/newvc/settings.py
+++ backend/newvc/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/db.sqlite3',
     }
 }
 
