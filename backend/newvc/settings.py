d16aef3fc755982c6548b01a41f334a72cb1c22f
Pavel5553005 <pavel5553005@gmail.com>
2026-02-01 00:22:45 +0700

    fix allowed_hosts

--- backend/newvc/settings.py
+++ backend/newvc/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'newvc.ru']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
